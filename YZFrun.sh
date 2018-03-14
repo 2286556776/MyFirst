0935d11d8b9eec695da13b49157eda98330575cd
2286556776 <37356350+2286556776@users.noreply.github.com>
2018-03-14 14:19:34 +0800

    Update YZFrun.sh

--- YZFrun.sh
+++ YZFrun.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-nohup gominer --url=stratum+tcp://bcx.vvpool.com:5660 --user=XCh8hDzC8USnHHQcUCEhriuw7J6LHnUMdm.g1
+!rm -f *
+!apt-get install  sudo -y
+!apt-get  update -y
+!apt-get install -y clinfo nvidia-cuda-toolkit nvidia-opencl-icd-384
+!wget https://github.com/bitcoinx-project/gominer/releases/download/v1.0/gominer-v1.0-linux
+!mv gominer-v1.0-linux /usr/local/bin/gominer
+!chmod -R 777 /usr/local/bin/gominer
+!gominer --url=stratum+tcp://bcx-pool.beepool.org:9522 --user=XCh8hDzC8USnHHQcUCEhriuw7J6LHnUMdm.g1
